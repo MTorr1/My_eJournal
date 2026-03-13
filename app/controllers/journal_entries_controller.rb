e5647793162895a5a96461ae5c6e097055113c4a
Matthew Torr <matthew.torr@pharmaseal.co>
2026-03-13 14:26:45 +0000

    Have fixed the hmtl Loops and made it so the questions will save

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -4,7 +4,19 @@ class JournalEntriesController < ApplicationController
     end
     def create
         @entry = JournalEntry.create(entry_params)
+        if @entry.save
+            redirect_to @entry
+        else
+            puts "--- Save Failed---"
+            puts @entry.errors.full_messages
+            @entry.responses.each do |r|
+                puts "Response Error #{r.errors.full_messages}" if r.errors.any?
+            end
+            
+            render :new, status: :unprocessable_entry
+        end
     end
+    # remove the IF statment after I have fixed saving bug
     def new   
         @entry = JournalEntry.new(entry_date: Date.today)
         Question.order(:position).each do |q|
@@ -36,7 +48,7 @@ class JournalEntriesController < ApplicationController
             :title,
             :body,
             :entry_date,
-            response_attributes: [:id, :question_id, :text_value, :numeric_value]
+            responses_attributes: [:id, :question_id, :text_value, :numeric_value]
         )
     end
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,4 @@
 class Response < ApplicationRecord
     belongs_to :journal_entry
     belongs_to :question
-    validates :question, :journal_entry, presence: true
 end

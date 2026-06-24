36dede3345a20fc92eaf2d6af1585a41a5f82b19
Matthew Torr <matthew.torr@pharmaseal.co>
2026-06-24 08:39:11 +0100

    added a log out button. Attempted to sign in with a new users and can see all of old users entries

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
-        
-    def create
-        @user = User.new(params[:user_id])
-        if @user.save
-            redirect_to @user
-        else
-            render "new"
-        end    
-    end
+    before_action :authenticate_user!
+        def create
+            @user = User.new(params[:user_id])
+            if @user.save
+                redirect_to @user
+            else
+                render "new"
+            end    
+        end
 
 
-    def new
-        @user = User.new
-    end
+        def new
+            @user = User.new
+        end
 # unsure if my naming conventions are correct here, Should this instyance veriable be @SighUp = SignUp.new?
 end
 
